c434624bf03f10b524ffc6d2acece5ca65fb1496
ilhwan <poj0722@naver.com>
2022-08-14 23:59:06 +0900

    :white_check_mark: HelloControllerTest 추가

---
+++ src/test/java/com/pppp0722/springbootdevopspractice/controller/HelloControllerTest.java
@@ -0,0 +1,31 @@
+package com.pppp0722.springbootdevopspractice.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("개인정보 조회 테스트")
+    void ME_TEST() throws Exception {
+        // given & when & then
+        mockMvc.perform(get("/hello"))
+            .andExpect(status().isOk());
+    }
+}
