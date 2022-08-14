5d2b468d94d0ce6a8a55f5e05cc285a2ba5c37b8
ilhwan <poj0722@naver.com>
2022-08-14 17:28:40 +0900

    :tada: 프로젝트 생성

---
+++ src/main/java/com/pppp0722/springbootdevopspractice/SpringbootDevopsPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.pppp0722.springbootdevopspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDevopsPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDevopsPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pppp0722/springbootdevopspractice/SpringbootDevopsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pppp0722.springbootdevopspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDevopsPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
